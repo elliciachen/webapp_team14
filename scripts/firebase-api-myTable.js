77202b3725fab4782d011da9cc66a0cd3f0af6f2
Ellicia Chen <echen67@my.bcit.ca>
2020-11-01 23:12:37 -0800

    Move Firebase config to index.html to see if it fixed the loading error.

--- scripts/firebase-api-myTable.js
+++ scripts/firebase-api-myTable.js
@@ -1,15 +1,15 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyBrQ1Y_P0vEZ7t22qKlD010XiXl_a32cOQ",
-  authDomain: "mytable-84f10.firebaseapp.com",
-  databaseURL: "https://mytable-84f10.firebaseio.com",
-  projectId: "mytable-84f10",
-  storageBucket: "mytable-84f10.appspot.com",
-  messagingSenderId: "426083113940",
-  appId: "1:426083113940:web:5cbc79c797a6f8459c5441",
-  measurementId: "G-T9D26YJST9",
-};
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBrQ1Y_P0vEZ7t22qKlD010XiXl_a32cOQ",
+//   authDomain: "mytable-84f10.firebaseapp.com",
+//   databaseURL: "https://mytable-84f10.firebaseio.com",
+//   projectId: "mytable-84f10",
+//   storageBucket: "mytable-84f10.appspot.com",
+//   messagingSenderId: "426083113940",
+//   appId: "1:426083113940:web:5cbc79c797a6f8459c5441",
+//   measurementId: "G-T9D26YJST9",
+// };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
 //firebase.analytics();
