1af9fa4d4026501e6e7924b0deb9d3380980896d
Fadi Basheer <fadih1992@gmail.com>
2021-06-03 18:07:14 -0700

    03-06-2021

--- public/scripts/firebase-api-myTable.js
+++ public/scripts/firebase-api-myTable.js
@@ -1,14 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyBrQ1Y_P0vEZ7t22qKlD010XiXl_a32cOQ",
-    authDomain: "mytable-84f10.firebaseapp.com",
-    databaseURL: "https://mytable-84f10.firebaseio.com",
-    projectId: "mytable-84f10",
-    storageBucket: "mytable-84f10.appspot.com",
-    messagingSenderId: "426083113940",
-    appId: "1:426083113940:web:5cbc79c797a6f8459c5441"
-};
+    apiKey: "AIzaSyBpYyhJ9OAZeE5Li7a0Ua_vcYQZBELGcsw",
+    authDomain: "project1-9dfa1.firebaseapp.com",
+    projectId: "project1-9dfa1",
+    storageBucket: "project1-9dfa1.appspot.com",
+    messagingSenderId: "237823476111",
+    appId: "1:237823476111:web:e30ad69206f63bf2288bfa"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
